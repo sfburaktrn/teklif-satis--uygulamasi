c98db22c4e521904258ba5e0e9f348d054057482
sfburaktrn <sbtorun.sbt@gmail.com>
2026-02-16 13:38:27 +0300

    feat: Add compiled backend application files, including an offers module, and a new frontend dashboard page, while updating the .gitignore.

--- backend/dist/offers/dto/update-offer.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateOfferDto } from './create-offer.dto';
-declare const UpdateOfferDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateOfferDto>>;
-export declare class UpdateOfferDto extends UpdateOfferDto_base {
-}
-export {};
